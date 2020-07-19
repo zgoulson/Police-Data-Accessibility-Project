27191a5f476a789522c47df3710f152388cebac1
zgoulson <zgoulson@gmail.com>
2020-07-19 14:18:14 -0700

    Adding sqlite db for tracking messages that have already been tagged

--- utils/slackbots/etiquette-bot/application.py
+++ utils/slackbots/etiquette-bot/application.py
@@ -28,31 +28,36 @@ slack_client = WebClient(SLACK_BOT_TOKEN)
 SLACK_SIGNING_SECRET = os.getenv('SLACK_SIGNING_SECRET')
 slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events")
 
-slack_admin = 'U014A5XS4JK' # this is currently Zach, but can be altered for any future admin or admin group
+# this is currently Zach, but can be altered for any future admin or admin group
+slack_admin = 'U014A5XS4JK'
 
 # Example reaction emoji echo
 @slack_events_adapter.on("reaction_added")
 def reaction_added(event_data):
     event = event_data["event"]
     emoji = event["reaction"]
-    if emoji == 'thread': # Only processes the bot response if the emoji is :thread:
+    if emoji == 'thread':  # Only processes the bot response if the emoji is :thread:
         channel = event["item"]["channel"]
         message_author = event["item_user"]
-        message_id = event["item"]["ts"].replace('.','') # remove . for URL compatibility
-        moderator = event["user"] # user who added the :thread: reaction
+        message_id = event["item"]["ts"].replace(
+            '.', '')  # remove . for URL compatibility
+        moderator = event["user"]  # user who added the :thread: reaction
         text = (f'Hello there! <@{moderator}> marked your recent message posted in <#{channel}> for cleanup. '
-        f'Please try to send only one message at a time or thread your messages to the initial message. '
-        f'Doing so will help keep our channels clean and digestible for all. Thanks! Please send a direct message to <@{slack_admin}> '
-        f'if you have any questions.\n\n'
-        f'https://policeaccessibility.slack.com/archives/{channel}/p{message_id}')
+                f'Please try to send only one message at a time or thread your messages to the initial message. '
+                f'Doing so will help keep our channels clean and digestible for all. Thanks! Please send a direct message to <@{slack_admin}> '
+                f'if you have any questions.\n\n'
+                f'https://policeaccessibility.slack.com/archives/{channel}/p{message_id}')
         slack_client.chat_postMessage(channel=message_author, text=text)
-        slack_client.chat_postMessage(channel=moderator, text=text) # for testing purposes. Sends duplicate message to Zach
-    print(event) # for testing purposes
+        # for testing purposes. Sends duplicate message to Zach
+        slack_client.chat_postMessage(
+            channel=moderator, text=f'original author was <@{message_author}>' + text)
+    print(event)  # for testing purposes
 
 # Error events
 @slack_events_adapter.on("error")
 def error_handler(err):
     print("ERROR: " + str(err))
 
+
 # Flask server with the default `/events` endpoint on port 3000
-slack_events_adapter.start(port=3000,debug=True)
+slack_events_adapter.start(port=3000, debug=True)
