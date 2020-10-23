9adf5e5a610020c50171f8d3bbe56ce80b393139
zgoulson <zgoulson@gmail.com>
2020-10-23 16:15:44 -0700

    Initializing scripts and raw .csv
    Initializing agency and jurisdiction export scripts and raw .csv data

---
+++ MuckRock/export_agencies.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import requests
+import pandas as pd
+import datetime
+import time
+
+from utils import get_api_key
+
+token = get_api_key()
+url = 'https://www.muckrock.com/api_v1/'
+
+headers = {'Authorization': 'Token %s' % token, 'content-type': 'application/json'}
+page_size = 200
+next_ = url + 'agency' + '/?page_size=%d' % (page_size)
+
+SUPRESS_ERRORS = False
+page = 1
+d=[]
+
+while next_ is not None:
+    r = requests.get(next_, headers=headers)
+    try:
+        json = r.json()
+        next_ = json['next']
+        d.append(r.json())
+        print('Page %d of %d' % (page, json['count'] / page_size + 1))
+        page += 1
+    except Exception as e:
+        print('Error ', e)
+        if not SUPRESS_ERRORS:
+            raise
+df = pd.json_normalize(data=d,max_level=0,record_path='results')
+
+df.to_csv('muckrock_agencies.csv', index=False)
---
+++ MuckRock/utils.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import requests
+from getpass import getpass
+
+# Using this method of secret storing:
+# https://stackoverflow.com/questions/25501403/storing-the-secrets-passwords-in-a-separate-file
+
+try:
+    import credentials
+    token = credentials.API_key
+except Exception as e:
+    print(e)
+    token = getpass()
+    credentials_file = open("credentials.py", "w")
+    credentials_file.writelines("API_key = \"" + token +"\"")
+    credentials_file.close
+
+
+API_URL = 'https://www.muckrock.com/api_v1/'
+
+def get_api_key():
+    return token
+
+def get_headers(token=None):
+    if token:
+        return {
+            'Authorization': 'Token %s' % token,
+            'content-type': 'application/json'
+        }
+    else:
+        return {'content-type': 'application/json'}
+
+def display_progress(current, total):
+    percent = (current / total) * 100.00
+    sys.stdout.write("\r%d%%" % percent)
+    sys.stdout.flush()
